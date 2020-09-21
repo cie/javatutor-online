ec25573ff41f7e8296fd1ff3c4d60baa57e8c7c7
Bernát Kalló <kallo.bernat@gmail.com>
2020-09-21 07:09:44 +0200

    webpack WIP 2

--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor'
 
+console.log('woohoo')
 Meteor.startup(() => {
+  console.log('yeah')
   // code to run on server at startup
-});
+})
