f810733e5c9331783bb0155f6137ba2d9138dcc8
Bernát Kalló <kallo.bernat@gmail.com>
2020-11-14 12:37:25 +0100

    test suite for hints

--- server/run.js
+++ server/run.js
@@ -1,36 +1,36 @@
-import fs, { mkdirSync } from 'fs'
-import child_process from 'child_process'
-import { promisify } from 'util'
+const fs = require('fs')
+const { mkdirSync } = fs
+const child_process = require('child_process')
+const { promisify } = require('util')
 
 const exec = promisify(child_process.exec)
 
-Meteor.methods({
-  async run({ code, input, student_id }) {
-    const dir = `/tmp/javatutor-${student_id}`
-    mkdirSync(dir, { recursive: true })
-    const m = code.match(/public\s+class\s+(\w+)/)
-    if (!m)
-      throw new Error(
-        'The code must contain a public class, e.g.\npublic class MyClass {\n    ...\n}'
-      )
-    const className = m[1]
-    fs.writeFileSync(`${dir}/${className}.java`, code)
-    try {
-      await exec(`javac ${className}.java`, { cwd: dir })
-    } catch (e) {
-      return e.stdout + '\n' + e.message
-    }
-    fs.writeFileSync(`${dir}/input.txt`, input)
-    const result = await new Promise(r =>
-      child_process.exec(
-        `java ${className} < input.txt`,
-        { cwd: dir, timeout: 3000, maxBuffer: 2 * 1024 },
-        (err, stdout, stderr) => {
-          r(stdout.toString() + (err ? '\n' + err.message : ''))
-        }
-      )
+export default async function run({ code, input, student_id }) {
+  const dir = `/tmp/javatutor-${student_id}`
+  mkdirSync(dir, { recursive: true })
+  const m = code.match(/public\s+class\s+(\w+)/)
+  if (!m)
+    throw new Error(
+      'The code must contain a public class, e.g.\npublic class MyClass {\n    ...\n}'
     )
-    console.log(result)
-    return result
+  const className = m[1]
+  fs.writeFileSync(`${dir}/${className}.java`, code)
+  try {
+    await exec(`javac ${className}.java`, { cwd: dir })
+  } catch (e) {
+    return e.stdout + '\n' + e.message
   }
-})
+  fs.writeFileSync(`${dir}/input.txt`, input)
+  const result = await new Promise(r =>
+    child_process.exec(
+      `java ${className} < input.txt`,
+      { cwd: dir, timeout: 3000, maxBuffer: 2 * 1024 },
+      (err, stdout, stderr) => {
+        r(stdout.toString() + (err ? '\n' + err.message : ''))
+      }
+    )
+  )
+  return result
+}
+
+if (typeof Meteor !== 'undefined') Meteor.methods({ run })
--- test/tasks.test.js
+++ test/tasks.test.js
@@ -5,8 +5,8 @@ const { safeLoad } = require('js-yaml')
 const assert = require('assert').strict
 const Ajv = require('ajv')
 const fs = require('fs')
-const mulang = require('mulang')
 const getHints = require('../server/getHints').default
+const run = require('../server/run').default
 
 const schema = JSON.parse(
   fs.readFileSync(__dirname + '/../tasks.schema.json').toString()
@@ -31,6 +31,43 @@ describe('tasks', () => {
             )
           })
         }
+        let code = task.initialCode
+        ;(task.hints || []).forEach((hint, i) => {
+          if (!hint.solution) return
+          const oldCode = code
+          const { turn, into } = hint.solution
+          const newCode = (code = oldCode.replace(turn, into))
+
+          const { message } = hint
+
+          it(message, async () => {
+            if (!~oldCode.indexOf(turn)) {
+              throw new Error(`Cannot find '${turn}' in code:\n${code}`)
+            }
+            const hintsBefore = await getHints(oldCode, task.id)
+            assert.equal(hintsBefore[0] && hintsBefore[0].message, message)
+            const hintsAfter = await getHints(newCode, task.id)
+            assert.notEqual(hintsAfter[0] && hintsAfter[0].message, message)
+
+            if (hint.solution.next) {
+              assert.equal(
+                hintsAfter[0] && hintsAfter[0].message,
+                task.hints[i + hint.solution.next].message
+              )
+            }
+          })
+
+          if (hint.solution.done) {
+            it('runs', async () => {
+              const output = await run({
+                code,
+                input: task.input,
+                student_id: 'test'
+              })
+              assert.equal(output.trim(), task.expectedOutput.trim())
+            })
+          }
+        })
       })
     }
   })
