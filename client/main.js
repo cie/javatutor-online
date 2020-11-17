d5ec53860dc18d964b513d56f71698b68a4fcf84
Bernát Kalló <kallo.bernat@gmail.com>
2020-11-17 10:28:01 +0100

    work on third task

--- client/main.js
+++ client/main.js
@@ -3,7 +3,7 @@ import '../imports/api'
 import App from '../imports/ui/App.svelte'
 import 'vscode'
 
-console.log('46')
+console.log('47')
 Meteor.startup(() => {
   new App({ target: document.getElementById('app') })
 })
